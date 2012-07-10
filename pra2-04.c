4911b61aad4e6ea9c4aa4f6a8867155f44fa62ec
dracher <dracher@gmail.com>
2012-07-10 17:51:57 +0800

    3-02

--- pra2-04.c
+++ pra2-04.c
@@ -5,24 +5,23 @@ int have_char(char c, char s[]);
 
 int main (int argc, char const *argv[])
 {
-	char s1[100] = "hello";
-	char s2[100] = "lo";
-	printf("result is %d\n", have_char('z', s1));
-	//squeeze(s1, s2);
-	//printf("%s\n", s1);
+	char s1[100] = "abcdefg";
+	char s2[100] = "de";
+	squeeze(s1, s2);
+	printf("%s\n", s1);
 	return 0;
 }
 
 void squeeze(char s[], char t[])
 {
-	int i, j, k;
+	int i, j;
 	i = j = 0;
 	for(i = 0; s[i]!='\0'; ++i)
 	{
-		if ()
-		{
-			;
-		}
+		if (have_char(s[i], t))
+			s[i] = s[++j];
+		else
+			s[i] = s[j++];
 	}
 	s[j] = '\0';
 }
